da60e65d08f69a0337cac68444afc1ed7a19ec88
Artem Zubkov <temazubkov02@gmail.com>
2022-02-15 09:48:37 +0200

    15.02.2022 finished Telegram Weather Bot

---
+++ Weather TG Bot/main.py
@@ -0,0 +1,43 @@
+import config   # You need use your Telegram Token and Weather Token
+import requests
+from datetime import datetime
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+
+bot = Bot(token=config.TOKEN_TELEGRAM_BOT)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def start_bot(message: types.Message):
+    await message.reply('Hello! Write the city where you want to know the weather: ')
+
+
+@dp.message_handler()
+async def get_weather(message: types.Message):
+    try:
+        r = requests.get(
+            f"http://api.openweathermap.org/data/2.5/weather?q="
+            f"{message.text}&appid={config.API_TOKEN_WEATHER}&units=metric"
+        )
+        data = r.json()
+
+        city = data['name']
+        temp = data['main']['temp']
+        temp_max = data['main']['temp_max']
+        temp_min = data['main']['temp_min']
+        wind = data['wind']['speed']
+        weather_description = data['weather'][0]['description']
+
+        await message.reply(f"{datetime.now().strftime('%d/%m/%Y %H:%M')}\nCity: {city}\n"
+                            f"Temperature: {temp}° {weather_description}\n"
+                            f"Maximum temperature: {temp_max}°\nMinimal temperature: {temp_min}°\n"
+                            f"Wind speed: {wind}\n")
+    except:
+        await message.reply("Write correct city name")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
